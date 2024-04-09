9a1565b1a5b10a975218a7b2673c39a304ce1c80
selemene112 <ariyanda98y@gmail.com>
2024-04-09 12:16:41 +0800

    add v1.0.0 Add validate

--- src/Controller/UserCOntroller/AuthUser.js
+++ src/Controller/UserCOntroller/AuthUser.js
@@ -5,21 +5,46 @@ const prisma = new PrismaClient();
 const { hashPassword, comparePassword } = require('../../Midleware/Bcrypt/encrypt');
 const jwt = require('jsonwebtoken');
 const secretKey = 'mysecretkey';
+const Joi = require('joi');
+const cloudinary = require('../../Config/Cloudinary');
 
 //=========================================== END Call Utiliy =================================================================
 
 //======================================================== Auth Register ======================================================
 
 const RegisterUserController = async (req, res) => {
-  const hashedPassword = await hashPassword(req.body.password);
-  let data = {
+  // return console.log(req.body);
+  const skema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+      .required(),
+    password: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).min(8),
+  });
+
+  const { error } = skema.validate({
     name: req.body.name,
     email: req.body.email,
-    password: hashedPassword,
-    photo: req.body.photo,
-  };
-
+    password: req.body.password,
+  });
+  if (error) {
+    return res.status(400).json({
+      message: error.message,
+      error: error,
+      data: null,
+    });
+  }
   try {
+    const cloudinaryProfilUser = await cloudinary.uploader.upload(req.file.path, {
+      folder: 'profil',
+    });
+    const hashedPassword = await hashPassword(req.body.password);
+    let data = {
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+      photo: cloudinaryProfilUser.url,
+    };
     const user = await prisma.user.create({
       data: data,
     });
@@ -30,8 +55,9 @@ const RegisterUserController = async (req, res) => {
     });
   } catch (error) {
     res.status(500).json({
-      message: 'something went wrong',
+      message: error.message,
       error: error,
+      data: null,
     });
   }
 };
@@ -39,6 +65,24 @@ const RegisterUserController = async (req, res) => {
 //========================================== Login Controller =================================================================
 const LoginUserControlller = async (req, res) => {
   const { email, password } = req.body;
+  const skema = Joi.object({
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+      .required(),
+    password: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+  });
+
+  const { error } = skema.validate({
+    email: email,
+    password: password,
+  });
+  if (error) {
+    return res.status(400).json({
+      message: error.message,
+      error: error,
+      data: null,
+    });
+  }
   try {
     // =========================================== Cek Email =================================================================
     const LoginUser = await prisma.user.findUnique({
--- src/Route/Testing.js
+++ src/Route/Testing.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const routeAuth = Router();
+const upload = require('../Midleware/Multer/UploadPhoto');
 
 const { RegisterUserController, LoginUserControlller } = require('../Controller/UserCOntroller/AuthUser');
 
-routeAuth.post('/register', RegisterUserController);
+routeAuth.post('/register', upload.single('photo'), RegisterUserController);
 routeAuth.post('/login', LoginUserControlller);
 
 module.exports = routeAuth;
